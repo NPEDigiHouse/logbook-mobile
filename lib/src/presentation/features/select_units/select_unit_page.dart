221116fe53ef528cf7bd721a6dd0714c2a9bad32
toku404 <phantom26isn@gmail.com>
2023-05-20 04:00:51 +0800

    feat: slicing select unit page

---
+++ lib/src/presentation/features/select_units/select_unit_page.dart
@@ -0,0 +1,80 @@
+import 'package:elogbook/core/helpers/app_size.dart';
+import 'package:elogbook/core/styles/color_palette.dart';
+import 'package:elogbook/core/styles/text_style.dart';
+import 'package:elogbook/src/presentation/features/select_units/widgets/custom_bottom_alert.dart';
+import 'package:elogbook/src/presentation/features/select_units/widgets/select_unit_card.dart';
+import 'package:flutter/material.dart';
+
+class SelectUnitPage extends StatefulWidget {
+  const SelectUnitPage({super.key});
+
+  @override
+  State<SelectUnitPage> createState() => _SelectUnitPageState();
+}
+
+class _SelectUnitPageState extends State<SelectUnitPage> {
+  ValueNotifier<int> _selectedIndex = ValueNotifier(0);
+
+  @override
+  void dispose() {
+    super.dispose();
+    _selectedIndex.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final dummyUnitName = [
+      "Obstetrics and Gynecology",
+      "Sattelite Hospital",
+      "Neuroopthalmology",
+      "Infection & Immunology",
+      "Vitro Retina",
+      "Glaucoma",
+      "Pediatric,Opthalmology, and Strabismus",
+      "Emergency Unit",
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Select Unit"),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.sort_by_alpha,
+              color: primaryColor,
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: ValueListenableBuilder(
+            valueListenable: _selectedIndex,
+            builder: (context, value, _) {
+              return ListView.separated(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+                separatorBuilder: (context, index) => SizedBox(
+                  height: 16,
+                ),
+                itemBuilder: (context, index) {
+                  return SelectUnitCard(
+                    unitName: dummyUnitName[index],
+                    index: index,
+                    value: value,
+                    selectedIndex: _selectedIndex,
+                  );
+                },
+                itemCount: dummyUnitName.length,
+              );
+            }),
+      ),
+    );
+  }
+}
---
+++ lib/src/presentation/features/select_units/widgets/select_unit_card.dart
@@ -0,0 +1,104 @@
+import 'package:elogbook/core/helpers/app_size.dart';
+import 'package:elogbook/core/styles/color_palette.dart';
+import 'package:elogbook/core/styles/text_style.dart';
+import 'package:elogbook/src/presentation/features/select_units/widgets/custom_bottom_alert.dart';
+import 'package:flutter/material.dart';
+
+class SelectUnitCard extends StatelessWidget {
+  const SelectUnitCard({
+    super.key,
+    required this.unitName,
+    required ValueNotifier<int> selectedIndex,
+    required this.value,
+    required this.index,
+  }) : _selectedIndex = selectedIndex;
+
+  final String unitName;
+  final ValueNotifier<int> _selectedIndex;
+  final int value;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: AppSize.getAppWidth(context),
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+          color: scaffoldBackgroundColor,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+                offset: Offset(0, 0),
+                spreadRadius: 0,
+                blurRadius: 6,
+                color: Color(0xFFD4D4D4).withOpacity(.25)),
+            BoxShadow(
+                offset: Offset(0, 4),
+                spreadRadius: 0,
+                blurRadius: 24,
+                color: Color(0xFFD4D4D4).withOpacity(.25)),
+          ]),
+      child: IntrinsicHeight(
+        child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 2),
+              width: 5,
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (value == index)
+                    Text(
+                      "Currently selected",
+                      style:
+                          textTheme.labelSmall?.copyWith(color: primaryColor),
+                    ),
+                  if (value == index)
+                    SizedBox(
+                      height: 2,
+                    ),
+                  Text(
+                    unitName,
+                    maxLines: 2,
+                    style: textTheme.bodyMedium?.copyWith(
+                      fontWeight: FontWeight.w600,
+                      height: 1.2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Radio.adaptive(
+              value: index,
+              groupValue: value,
+              onChanged: (v) {
+                _selectedIndex.value = v!;
+                showModalBottomSheet(
+                  context: context,
+                  backgroundColor: Colors.transparent,
+                  builder: (BuildContext context) {
+                    return CustomBottomAlert(
+                        message: 'Successfully replaced unit!');
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
