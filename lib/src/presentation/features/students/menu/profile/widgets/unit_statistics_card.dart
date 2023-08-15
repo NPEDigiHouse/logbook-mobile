5f1782c1fc224e738dcd8380c6b00fbbb3046856
fajri-rasid1st <fajrimuhammad460@gmail.com>
2023-08-15 17:14:29 +0800

    feat: finish unit statistics ui

---
+++ lib/src/presentation/features/students/menu/profile/widgets/unit_statistics_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:elogbook/core/styles/color_palette.dart';
+
+class UnitStatisticsCard extends StatelessWidget {
+  final double width;
+  final EdgeInsetsGeometry? padding;
+  final Widget? child;
+
+  const UnitStatisticsCard({
+    super.key,
+    this.width = double.infinity,
+    this.padding,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      padding: padding,
+      decoration: BoxDecoration(
+        color: scaffoldBackgroundColor,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            offset: const Offset(0, 1),
+            blurRadius: 16,
+            color: Colors.black.withOpacity(.1),
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
